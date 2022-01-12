81fcafae32c276bfa7d108bbd4a93e2da74c36ea
Adam Homer <adamhomer88@gmail.com>
2022-01-11 22:36:58 -0800

    Initial project. First class.

---
+++ GardenersMultitool.Domain/ValueObjects/Temperature.cs
@@ -0,0 +1,28 @@
+﻿using CSharpFunctionalExtensions;
+using System.Collections.Generic;
+
+namespace GardenersMultitool.Domain.ValueObjects
+{
+    public enum TemperatureUnit
+    {
+        Farenheit,
+        Celsius
+    }
+
+    public class Temperature : ValueObject
+    {
+        public decimal Value { get; }
+        public TemperatureUnit Unit { get; }
+     
+        public Temperature(decimal value, TemperatureUnit unit)
+        {
+            Value = value;
+            Unit = unit;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Unit;
+        }
+    }
+}
