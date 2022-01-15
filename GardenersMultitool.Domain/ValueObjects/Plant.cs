e46f512d9810981b3b3c2ee34e5db23b07126ef6
Zain890 <Zain890@gmail.com>
2022-01-15 00:58:32 -0800

    PlantDataImporter Added.

--- GardenersMultitool.Domain/ValueObjects/Plant.cs
+++ GardenersMultitool.Domain/ValueObjects/Plant.cs
@@ -1,12 +1,66 @@
 ﻿using CSharpFunctionalExtensions;
+using CsvHelper.Configuration.Attributes;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace GardenersMultitool.Domain.ValueObjects
 {
     public class Plant : ValueObject
     {
-        public string Name { get; }
+        //Properties modelled after .csv
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Name("Scientific name")]
+        public string ScientificName { get; set; }
+        public string Binomial { get; set; }
+        [Name("Plant Type")]
+        public string PlantType { get; set; }
+        public string Height { get; set; }
+        public string Spread { get; set; }
+        [Name("Root Depth")]
+        public string RootDepth { get; set; }
+        [Name("Seasonal Interest")]
+        public string SeasonalInterest { get; set; }
+        public string Notes { get; set; }
+        [Name("Flower Color")]
+        public string FlowerColor { get; set; }
+        [Name("Root Type")]
+        public string RootType { get; set; }
+        [Name("Bloom Time")]
+        public string BloomTime { get; set; }
+        [Name("Fruit Time")]
+        public string FruitTime { get; set; }
+        public string Texture { get; set; }
+        public string Form { get; set; }
+        [Name("Growth Rate")]
+        public string GrowthRate { get; set; }
+        [Name("Insect Predation")]
+        public string InsectPredation { get; set; }
+        public string Disease { get; set; }
+        [Name("Light")]
+        public string LightRequired{ get; set; }
+        [Name("Hardiness Zone")]
+        public string HardinessZone { get; set; }
+        [Name("Soil Moisture")]
+        public string SoilMoisture { get; set; }
+        [Name("Soil pH")]
+        public string SoilPH { get; set; }
+        [Name("Ecological Function")]
+        public string EcologicalFunction { get; set; }
+        [Name("Human Use/Crop")]
+        public string HumanUseCrop { get; set; }
+
         public PlotNeeds Functions { get; }
+
+        public override string ToString()
+        {
+            return $"Object Id:{Id}, Name: {Name}.";
+        }
+
+        public Plant()
+        {
+
+        }
 
         public Plant(string name, PlotNeeds needs)
         {
@@ -16,7 +70,34 @@ namespace GardenersMultitool.Domain.ValueObjects
 
         protected override IEnumerable<object> GetEqualityComponents()
         {
+            yield return Id;
             yield return Name;
+            //yield return Height;
+            //yield return ScientificName;
+            //yield return Binomial;
+            //yield return PlantType;
+            //yield return Height;
+            //yield return Spread;
+            //yield return RootDepth;
+            //yield return SeasonalInterest;
+            //yield return Notes;
+            //yield return FlowerColor;
+            //yield return RootType;
+            //yield return BloomTime;
+            //yield return FruitTime;
+            //yield return Texture;
+            //yield return Form;
+            //yield return GrowthRate;
+            //yield return InsectPredation;
+            //yield return Disease;
+            //yield return LightRequired;
+            //yield return HardinessZone;
+            //yield return SoilMoisture;
+            //yield return SoilPH;
+            //yield return EcologicalFunction;
+            //yield return HumanUseCrop;
+            //yield return PlotNeeds;
+
         }
     }
 }
---
+++ PlantDataImporter/Program.cs
@@ -0,0 +1,75 @@
+﻿using CsvHelper;
+using GardenersMultitool.Domain.ValueObjects;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace PlantDataImporter
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //if (args.Length == 0)
+            //    return;
+
+            //var path = args[0];
+
+            //if (string.IsNullOrEmpty(path))
+            //    return;
+
+
+            //make list of file names (Annuals, aquatics, etc) iterate through list[]
+            var csvFolder = @"D:\ZainGit\RedSyndicateStuff\PermacultureData\CSV";
+            var jsonFolder = @"D:\ZainGit\RedSyndicateStuff\PermacultureData\JSON";
+            var files = Directory.GetFiles(csvFolder);
+
+            //make records list
+            var plants = new List<Plant>();
+
+
+            //json serialize the records list (collection)
+            foreach (var file in files)
+            {
+
+                using (var reader = new StreamReader(Path.Combine(csvFolder, file)))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<Plant>();
+                    foreach (var record in records)
+                    {
+                        plants.Add(record);
+                        Console.WriteLine($"{record.Name}");                       
+                    }
+                 }
+                
+            }
+
+            var json = JsonConvert.SerializeObject(plants);
+            var allPlants = JsonConvert.DeserializeObject<List<Plant>>(json);
+
+
+            File.WriteAllText(Path.Combine(jsonFolder, "plants.json"), json);
+
+
+            Console.WriteLine(allPlants[0].ToString());
+            Console.WriteLine(allPlants[1].ToString());
+            Console.WriteLine(allPlants[2].ToString());
+
+            Console.ReadLine();
+
+
+
+            //Next Step: For each different value of a property (like PlantType), list out each one once, and log it to console, so I can get a list of all enums to add to .cs files
+
+        }
+
+
+
+
+    }
+}
