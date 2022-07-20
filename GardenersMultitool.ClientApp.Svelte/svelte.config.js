a9370a60b84b5e0cad93315ff75b58f2f5782df7
KungRaseri <me@kungraseri.ninja>
2022-07-20 14:07:52 -0700

    added playwright, added tailwindcss

--- GardenersMultitool.ClientApp.Svelte/svelte.config.js
+++ GardenersMultitool.ClientApp.Svelte/svelte.config.js
@@ -1,7 +1,13 @@
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss';
 import sveltePreprocess from 'svelte-preprocess'
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: sveltePreprocess({
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()]
+    }
+  })
 }
