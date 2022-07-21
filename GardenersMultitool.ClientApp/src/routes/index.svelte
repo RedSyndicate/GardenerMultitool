d51edfe3af28c4b445529ebe58f4e927354f264b
KungRaseri <me@kungraseri.ninja>
2022-07-21 14:52:50 -0700

    before migration

---
+++ GardenersMultitool.ClientApp/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { each, onMount } from 'svelte/internal';
+	import { locations, getLocations } from '../lib/location';
+	onMount(() => {
+		getLocations();
+	});
+</script>
+
+<svelte:head>
+	<title>Gardener Multitool</title>
+</svelte:head>
+
+<h1>Dashboard</h1>
+ 
