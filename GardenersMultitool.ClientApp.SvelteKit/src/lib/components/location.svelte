491a63489151f8387d01c05bd52fb8629964861c
KungRaseri <me@kungraseri.ninja>
2022-12-16 13:56:03 -0800

    resolving vulnerabilities, etc.

---
+++ GardenersMultitool.ClientApp.SvelteKit/src/lib/components/location.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let location;
+</script>
+
+<div
+	class="
+	flex p-6 bg-gray-100 text-gray-800 
+	text-center rounded-md 
+	shadow-sm hover:shadow-md flex-col items-center"
+>
+	<img
+		class="h-40"
+		src={`https://via.placeholder.com/460x640/333333/cccccc?text=${location.name.value}`}
+		alt={location.name.value}
+	/>
+	{location.name.value}
+</div>
+
+<slot />
---
+++ GardenersMultitool.ClientApp.SvelteKit/src/lib/components/plant.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let plant;
+</script>
+
+<div
+	class="
+	flex p-6 bg-gray-100 text-gray-800 
+	text-center rounded-md 
+	shadow-sm hover:shadow-md flex-col items-center"
+>
+	<img
+		class="h-40"
+		src={`https://via.placeholder.com/460x640/333333/cccccc?text=${plant.name.value}`}
+		alt={plant.name.value}
+	/>
+	{plant.name.value}
+</div>
+
+<slot />
